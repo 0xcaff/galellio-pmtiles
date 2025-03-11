1988291104431c572c2421412495639578a70449
martin <martincharles07@gmail.com>
2025-03-11 16:27:37 -0500

    initial commit
    it builds

---
+++ src/main.rs
@@ -0,0 +1,163 @@
+//! This exmpale shows how to create and work with vector tile layers.
+
+use std::sync::Arc;
+
+use eframe::CreationContext;
+use galileo::control::{EventPropagation, MouseButton, UserEvent, UserEventHandler};
+use galileo::layer::vector_tile_layer::style::VectorTileStyle;
+use galileo::layer::vector_tile_layer::VectorTileLayerBuilder;
+use galileo::layer::VectorTileLayer;
+use galileo::tile_schema::{TileIndex, TileSchema, VerticalDirection};
+use galileo::{Lod, Map, MapBuilder};
+use galileo_egui::{EguiMap, EguiMapState};
+use galileo_types::cartesian::{Point2, Rect};
+use galileo_types::geo::Crs;
+use parking_lot::RwLock;
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    run()
+}
+
+struct App {
+    map: EguiMapState,
+    layer: Arc<RwLock<VectorTileLayer>>,
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            EguiMap::new(&mut self.map).show_ui(ui);
+        });
+
+        egui::Window::new("Buttons")
+            .title_bar(false)
+            .show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    if ui.button("Gray style").clicked() {
+                        self.set_style(gray_style());
+                    }
+                });
+            });
+    }
+}
+
+impl App {
+    fn new(
+        map: Map,
+        layer: Arc<RwLock<VectorTileLayer>>,
+        cc: &CreationContext,
+        handler: impl UserEventHandler + 'static,
+    ) -> Self {
+        Self {
+            map: EguiMapState::new(
+                map,
+                cc.egui_ctx.clone(),
+                cc.wgpu_render_state.clone().expect("no render state"),
+                [Box::new(handler) as Box<dyn UserEventHandler>],
+            ),
+            layer,
+        }
+    }
+
+    fn set_style(&mut self, style: VectorTileStyle) {
+        let mut layer = self.layer.write();
+        if style != *layer.style() {
+            layer.update_style(style);
+            self.map.request_redraw();
+        }
+    }
+}
+
+pub(crate) fn run() {
+    let layer = VectorTileLayerBuilder::new_rest(move |&index: &TileIndex| {
+        format!(
+            "https://api.maptiler.com/tiles/v3-openmaptiles/{z}/{x}/{y}.pbf",
+            z = index.z,
+            x = index.x,
+            y = index.y
+        )
+    })
+        .with_tile_schema(tile_schema())
+        .with_file_cache_checked(".tile_cache")
+        .with_attribution(
+            "© MapTiler© OpenStreetMap contributors".to_string(),
+            "https://www.maptiler.com/copyright/".to_string(),
+        )
+        .build()
+        .expect("failed to create layer");
+
+    let layer = Arc::new(RwLock::new(layer));
+
+    let layer_copy = layer.clone();
+    let handler = move |ev: &UserEvent, map: &mut Map| match ev {
+        UserEvent::Click(MouseButton::Left, mouse_event) => {
+            let view = map.view().clone();
+            if let Some(position) = map
+                .view()
+                .screen_to_map(mouse_event.screen_pointer_position)
+            {
+                let features = layer_copy.read().get_features_at(&position, &view);
+
+                for (layer, feature) in features {
+                    println!("{layer}, {:?}", feature.properties);
+                }
+            }
+
+            EventPropagation::Stop
+        }
+        _ => EventPropagation::Propagate,
+    };
+
+    let map = MapBuilder::default().with_layer(layer.clone()).build();
+    galileo_egui::init_with_app(Box::new(|cc| {
+        Ok(Box::new(App::new(map, layer, cc, handler)))
+    }))
+        .expect("failed to initialize");
+}
+
+fn gray_style() -> VectorTileStyle {
+    let style_str = r##"
+{
+  "rules": [
+  ],
+  "background": "#ffffffff",
+  "default_symbol": {
+    "line": {
+      "stroke_color": "#000000ff",
+      "width": 0.5
+    },
+    "polygon": {
+      "fill_color": "#999999ff"
+    }
+  }
+}"##;
+    serde_json::from_str(style_str).expect("invalid style json")
+}
+
+fn tile_schema() -> TileSchema {
+    const ORIGIN: Point2 = Point2::new(-20037508.342787, 20037508.342787);
+    const TOP_RESOLUTION: f64 = 156543.03392800014 / 4.0;
+
+    let mut lods = vec![Lod::new(TOP_RESOLUTION, 0).expect("invalid config")];
+    for i in 1..16 {
+        lods.push(
+            Lod::new(lods[(i - 1) as usize].resolution() / 2.0, i).expect("invalid tile schema"),
+        );
+    }
+
+    TileSchema {
+        origin: ORIGIN,
+        bounds: Rect::new(
+            -20037508.342787,
+            -20037508.342787,
+            20037508.342787,
+            20037508.342787,
+        ),
+        lods: lods.into_iter().collect(),
+        tile_width: 1024,
+        tile_height: 1024,
+        y_direction: VerticalDirection::TopToBottom,
+        crs: Crs::EPSG3857,
+    }
+}
